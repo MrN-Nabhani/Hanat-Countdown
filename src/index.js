ff63dda876dfa7bc416b86c3e86a582b78d1c4c8
MrN-Nabhani <nabhaninajm@gmail.com>
2021-03-05 17:44:29 +0200

    added themes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Themes from "./themes";
+import { GlobalStyles } from "./themes/GlobalStyles";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={Themes}>
+    <GlobalStyles />
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
