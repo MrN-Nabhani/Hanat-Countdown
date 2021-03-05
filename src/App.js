d2a9578a8d698fef527127dfec91fa46ef5a4bd9
maherAlzubi <invinity2017@gmail.com>
2021-03-05 20:36:10 +0200

    added dashboard to route

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard.js";
 
 class App extends Component {
   render() {
     return (
-       <Router>
-        <Switch>
-          <Route exact path="/" component={Signup} />
-          <Route path="/login" component={Login} />
-        </Switch>
-      </Router>
+      <Dashboard />
     );
   }
 }
 
 export default App;
+
+/*
+
+ <Router>
+        <Switch>
+          <Route exact path="/" component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Router>*/
---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Cards from "../../components/Dashboard/Cards/Cards.js";
+export default class Dashboard extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <Cards />
+    }
+}
